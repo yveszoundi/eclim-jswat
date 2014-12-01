753b55fc6dd84899e4bf28574cf657fec7cb11c7
Yves Zoundi <yveszoundi@gmail.com>
2014-12-01 08:29:05 -0500

    Code cleanup.

--- eclim-jswat.el
+++ eclim-jswat.el
@@ -2,8 +2,7 @@
 
 ;; Works only with JSwat 2.x
 
-(require 'eclim)
-(require 'cl-lib)
+(mapc #'require '(eclim cl-lib))
 
 (defgroup eclim-jswat nil
   "JSwat Options"
@@ -20,51 +19,50 @@
       ";"
     ":"))
 
-(defun eclim-jswat-buffer-name (mode)
+(defconst eclim-jswat-main-class-setting "org.eclim.java.run.mainclass")
+
+(defun eclim-jswat--buffer-name (mode)
   "*Eclim-Jswat*")
 
-(defun eclim-jswat-launch (cmd)
-  (compilation-start cmd
-                     'compilation-mode
-                     'eclim-jswat-buffer-name))
+(defun eclim-jswat--launch (cmd)
+  (compilation-start cmd 'compilation-mode 'eclim-jswat--buffer-name))
 
-(defun eclim-jswat-project-classpath ()
+(defun eclim-jswat--project-classpath ()
   (eclim/java-classpath eclim--project-name))
 
-(defun eclim-jswat-main-run-class ()
-  (eclim/project-setting eclim--project-name "org.eclim.java.run.mainclass"))
+(defun eclim-jswat--main-run-class ()
+  (eclim/project-setting eclim--project-name eclim-jswat-main-class-setting))
 
-(defun eclim-jswat-dir-path (p-root &rest path-elements)
+(defun eclim-jswat--dir-path (p-root &rest path-elements)
   (cl-reduce '(lambda (x &optional y)
                 (concat (file-name-as-directory x) y))
              path-elements :initial-value p-root))
 
-(defun eclim-jswat-project-src-folder ()
-  (let* ((eclim-prj-classpath-file (eclim-jswat-dir-path (eclim--project-dir) ".classpath"))
-	 (eclim-prj-classpath (xml-parse-file eclim-prj-classpath-file))
-       (eclim-classpath-entries (xml-get-children (car eclim-prj-classpath) 'classpathentry))
-       (eclim-prj-sources (mapcar #'(lambda (cp-entry)
-				      (progn
-                                      (let ((cp-kind (cdr (last (car (second cp-entry))))))
-                                        (when (string= cp-kind "src")
-                                           (cdr (assoc 'path (last (car (last cp-entry)))))))))
-                                  eclim-classpath-entries)))
-    (eclim-jswat-dir-path (eclim--project-dir) (car (remove-if 'null eclim-prj-sources)))))
+(defun eclim-jswat--project-src-path ()
+  (let* ((eclim-prj-classpath-file (eclim-jswat--dir-path (eclim--project-dir) ".classpath"))
+         (eclim-prj-classpath (xml-parse-file eclim-prj-classpath-file))
+         (eclim-classpath-entries (xml-get-children (car eclim-prj-classpath) 'classpathentry))
+         (eclim-prj-sources (mapcar #'(lambda (cp-entry-def)
+                                        (let ((cp-entry (second cp-entry-def)))
+                                          (when (string= "src" (cdr (assoc 'kind cp-entry)))
+                                            (eclim-jswat--dir-path (eclim--project-dir) (cdr (assoc 'path cp-entry))))))
+                                    eclim-classpath-entries))
+         (non-null-eclim-prj-sources (cl-remove-if #'null eclim-prj-sources)))
+    (mapconcat #'identity non-null-eclim-prj-sources ":")))
 
-(defun eclim-jswat-make-command ()
-  (let* ((java-vm (eclim-jswat-dir-path (getenv "JAVA_HOME") "bin" "java"))
-         (jpda-jar (eclim-jswat-dir-path (getenv "JAVA_HOME") "lib" "tools.jar"))
-         (java-src-zip (eclim-jswat-dir-path (getenv "JAVA_HOME") "src.zip"))
+(defun eclim-jswat--make-command ()
+  (let* ((java-vm (eclim-jswat--dir-path (getenv "JAVA_HOME") "bin" "java"))
+         (jpda-jar (eclim-jswat--dir-path (getenv "JAVA_HOME") "lib" "tools.jar"))
+         (java-src-zip (eclim-jswat--dir-path (getenv "JAVA_HOME") "src.zip"))
          (jswat-cmd (concat (file-name-as-directory jswat-path) "jswat"))
-         (proj-sourcepath (eclim-jswat-project-src-folder))
-         (jswat-src-path (concat java-src-zip eclim-jswat-classpath-separator proj-sourcepath))
-         (jswat-class-path (concat java-src-zip eclim-jswat-classpath-separator (eclim-jswat-project-classpath))))
+         (jswat-src-path (concat java-src-zip eclim-jswat-classpath-separator (eclim-jswat--project-src-path)))
+         (jswat-class-path (concat java-src-zip eclim-jswat-classpath-separator (eclim-jswat--project-classpath))))
     (concat
      jswat-cmd
      " 'sourcepath \"" jswat-src-path "\""
      "; classpath \"" jswat-class-path "\""
-     "; view \"" (eclim-jswat-main-run-class) "\""
-     "; run \"" (eclim-jswat-main-run-class) "\"'")))
+     "; view \"" (eclim-jswat--main-run-class) "\""
+     "; run \"" (eclim-jswat--main-run-class) "\"'")))
 
 ;;;###autoload
 (defun eclim-jswat-run ()
@@ -73,7 +71,7 @@
   (interactive)
   ;; Java interpreter
   (progn
-    (eclim-jswat-launch (eclim-jswat-make-command))))
+    (eclim-jswat--launch (eclim-jswat--make-command))))
 
 (provide 'eclim-jswat)
 
